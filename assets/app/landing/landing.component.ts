d9051232b4faad63d752836a2e059527e410b4be
Foshkey <foshkey@gmail.com>
2016-10-05 20:24:41 -0500

    Adjustments to loading screen

--- assets/app/landing/landing.component.ts
+++ assets/app/landing/landing.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'landing',
-  templateUrl: 'landing.component.html'
+  templateUrl: 'landing.component.html',
+  styleUrls: ['landing.component.css']
 })
 export class LandingComponent implements OnInit {
   constructor() { }
