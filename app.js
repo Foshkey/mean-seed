66f731544d3caa82b4472af4347943241828aef2
Joshua Nelson <foshkey@gmail.com>
2016-10-19 15:13:14 -0500

    Added winston logger

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 let express = require('express');
 let path = require('path');
 let favicon = require('serve-favicon');
-let logger = require('morgan');
+let morgan = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
+let logger = require('./logger');
 let routes = require('./routes/index');
 
 let app = express();
 
 // view engine setup
+const viewEngine = 'hbs';
+logger.debug(`Using view engine: ${viewEngine}`);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', viewEngine);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+logger.debug('Overriding express logger');
+let morganFormat = {
+  remoteAddr: ':remote-addr',
+  remoteUser: ':remote-user',
+  method: ':method',
+  url: ':url',
+  httpVersion: ':http-version',
+  status: ':status',
+  resultLength: ':res[content-length]',
+  referrer: ':referrer',
+  userAgent: ':user-agent',
+  responseTime: ':response-time'
+};
+app.use(morgan(JSON.stringify(morganFormat), { stream: logger.morganStream }));
+
+logger.debug('Setting up bodyParser & cookieParser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// static path
+const staticPath = path.join(__dirname, 'public');
+logger.debug(`Static path used: ${staticPath}`)
+app.use(express.static(staticPath));
 
 app.use('/', routes);
 
@@ -36,6 +58,7 @@ app.use((req, res, next) => {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use((err, req, res, next) => {
+    logger.error(err.message, err);
     res.status(err.status || 500);
     res.json({
       message: err.message,
@@ -47,6 +70,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
+  logger.error(err.message, err);
   res.status(err.status || 500);
   res.json({
     message: err.message,
